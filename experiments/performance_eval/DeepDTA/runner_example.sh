1ee0eca463d22ce263a13784b8626e9a82b2e083
Marco Frasca <frasca@di.unimi.it>
2021-09-06 21:14:03 +0200

    Update runner_example.sh

--- experiments/performance_eval/DeepDTA/runner_example.sh
+++ experiments/performance_eval/DeepDTA/runner_example.sh
@@ -5,7 +5,7 @@
 python compression.py --compression uECSQ --net original_nets/deepDTA_davis.h5 --dataset DAVIS --clusterfc 16 --clustercnn 128
 
 # This example applies pruning and uCWS quantization to the DeepDTA model trained on the Kiba dataset.
-# Pruning is only applied to fully conected layers (percentage 60%), while clustering is applied to 
+# Pruning is only applied to fully connected layers (percentage 60%), while clustering is applied to 
 # fully connected (k=32) and convolutional (k ranging from 16 to 128) layers
 for k in 16 32 64 96 128
 do
